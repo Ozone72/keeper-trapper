567a5597734bf3158e555aa9770ad63fa4c96ad3
Orin Fletcher <orinf@yahoo.com>
2020-12-24 03:58:38 +0000

    initial commit

---
+++ build/precache-manifest.18c70d0cb9c018871299841fcc5df1b2.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78549c2062233ee9636898151c91f316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533b3380298681bdc9dd",
+    "url": "/static/js/main.e9aa682b.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
